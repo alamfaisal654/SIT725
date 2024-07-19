94b90c6034cd9c137d7508a822d48677b32b31c1
alamfaisal654 <alamfaisal654@gmail.com>
2024-07-19 15:13:48 +1000

    All

---
+++ Week-2/server_get.js
@@ -0,0 +1,25 @@
+    const express= require("express");
+    const app= express();
+    app.use(express.static('public'))
+
+    const addTwoNumber= (n1,n2) => {
+        return n1+n2;
+    }
+
+    app.get("/addTwoNumber", (req,res)=>{
+        const n1= parseInt(req.query.n1);
+        const n2=parseInt(req.query.n2);
+        const result = addTwoNumber(n1,n2);
+        res.json({statuscocde:200, data: result }); 
+    });
+
+    app.get("/Display", (req, res) => {
+        const n1 = "<html><body><H1>HELLO THERE </H1></body></html>";
+        res.set('Content-Type', 'text/html');
+        res.send(Buffer.from(n1));     
+    })
+    console.log (addTwoNumber(19,12));
+    const port=3040;
+    app.listen(port,()=> {
+        console.log("hello i'm listening to port "+port);
+    })
